9f4cc7ee079925047d87f5174f72d5127e0eb6b2
MasterDan <MihaylovDS@gmail.com>
2021-05-10 19:16:33 +0300

    Still Drafting)

--- NuGen.Services.Fsharp/UniqCheckSimple.fs
+++ NuGen.Services.Fsharp/UniqCheckSimple.fs
@@ -2,6 +2,8 @@ module NuGen.Services.Fsharp.UniqCheckSimple
 
 open NuGen.Services.Services.Interfaces
 
-type UniqCheckFsharp() = interface IUniqCheckService with
-                             member this.CheckUniquenessAsync(value) = failwith "todo"
+type UniqCheckSimpleFsharp() =
+    let _cache = []
 
+    interface IUniqCheckService with
+        member this.CheckUniquenessAsync(value) = failwith "todo"

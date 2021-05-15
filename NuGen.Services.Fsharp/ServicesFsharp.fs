707f29f8fefba180f11595d05fb08811d011ded8
MasterDan <MihaylovDS@gmail.com>
2021-05-15 15:43:09 +0300

    To Generic list

--- NuGen.Services.Fsharp/ServicesFsharp.fs
+++ NuGen.Services.Fsharp/ServicesFsharp.fs
@@ -1,13 +1,17 @@
 module NuGen.Services.Fsharp.ServicesFsharp
 
+open System.Collections.Generic
+open Microsoft.Extensions.Options
+open NuGen.Options.Start
 open NuGen.Services.Interfaces
 
-type UniqCheckSimple() =
-    let mutable _cache = [];
+type UniqCheckSimple(opts: IOptions<StartOptions>) =
+    let options = opts.Value;
+    let _cache = List()
     interface IUniqCheckService with
         member this.CheckUniquenessAsync(value) =
-            if List.contains value _cache then
-                _cache <- _cache @ [value]
+            if _cache.Contains value then
+                _cache.Add value
                 async { return true } |> Async.StartAsTask
             else
                 async { return true } |> Async.StartAsTask

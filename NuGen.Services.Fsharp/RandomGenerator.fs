08192d7a992107ad3e54375bb69aac3916fd12b1
MasterDan <MihaylovDS@gmail.com>
2021-05-16 16:05:05 +0300

    Still Drafting

--- NuGen.Services.Fsharp/RandomGenerator.fs
+++ NuGen.Services.Fsharp/RandomGenerator.fs
@@ -1,8 +1,23 @@
 namespace NuGen.Services.Fsharp
 
-open NuGen.Services.Interfaces
+open System
+open Microsoft.Extensions.Options
+open NuGen.Options.Start
 open NuGen.Services.Interfaces
 
-type RandomGenerator(check: IUniqCheckService) =
+type RandomGenerator(check: IUniqCheckService, opts: IOptions<StartOptions>) =
+    let rnd = Random()
+
     interface IRandomGeneratorService with
-        member this.GenerateUniqueNumbersAsync(beginNumber, endNumber) = failwith "todo"
+        member this.GenerateUniqueNumbersAsync(beginNumber, endNumber) =
+            let maxNumber =
+                int (
+                    Math.Pow(10.0, float opts.Value.NumberOfDigits)
+                    - 1.0
+                )
+
+            for i in opts.Value.From.Value .. opts.Value.To.Value do
+                let value = rnd.Next(0, maxNumber)
+                
+
+            failwith "todo"

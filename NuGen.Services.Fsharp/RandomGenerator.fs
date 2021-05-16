514baf2b8dc1b9a320dcfd71e772eabb81553598
MasterDan <MihaylovDS@gmail.com>
2021-05-16 16:26:37 +0300

    trying async task call

--- NuGen.Services.Fsharp/RandomGenerator.fs
+++ NuGen.Services.Fsharp/RandomGenerator.fs
@@ -7,14 +7,17 @@ open NuGen.Services.Interfaces
 
 type RandomGenerator(check: IUniqCheckService, opts: IOptions<StartOptions>) =
     let rnd = Random()
+    let maxNumber = int (Math.Pow(10.0, float opts.Value.NumberOfDigits) - 1.0)
+    
+    let getNumberAsync =
+        while true do
+            let value = int64 (rnd.Next(0,maxNumber))
+            let isUnique = async { return check.CheckUniquenessAsync(value) }
+        
 
     interface IRandomGeneratorService with
         member this.GenerateUniqueNumbersAsync(beginNumber, endNumber) =
-            let maxNumber =
-                int (
-                    Math.Pow(10.0, float opts.Value.NumberOfDigits)
-                    - 1.0
-                )
+            
 
             for i in opts.Value.From.Value .. opts.Value.To.Value do
                 let value = rnd.Next(0, maxNumber)
